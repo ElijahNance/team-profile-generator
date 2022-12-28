96b0d09bbae306fad0dfc1915357797a0ff671a5
unknown <jesseelijah.nance@gmail.com>
2022-12-28 15:24:42 -0500

    completed employee test

--- Develop/__tests__/Employee.test.js
+++ Develop/__tests__/Employee.test.js
@@ -30,4 +30,23 @@ describe("Employee", () => {
   
         expect(typeof obj.getId).toBe("function");
     })
+
+    it("should return an object containing a 'getEmail' method when called with the 'new' keyword", () => {
+        const obj = new Employee();
+  
+        expect(typeof obj.getEmail).toBe("function");
+    })
+
+    it("should return an object containing a 'getRole' method when called with the 'new' keyword", () => {
+        const obj = new Employee();
+  
+        expect(typeof obj.getRole).toBe("function");
+    })
+
+    it("should return the string 'Employee' when called from a new instance of Employee", () => {
+        const obj = new Employee();
+  
+        expect(obj.getRole()).toBe('Employee');
+    })
+
 });
