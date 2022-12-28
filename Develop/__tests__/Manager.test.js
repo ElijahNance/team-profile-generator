74916b0ddac6d747612e00fd8518f4af65df3029
unknown <jesseelijah.nance@gmail.com>
2022-12-28 15:46:17 -0500

    Created manager test, added code to employee/ Manager classes

--- Develop/__tests__/Manager.test.js
+++ Develop/__tests__/Manager.test.js
@@ -1,6 +1,59 @@
 const Manager = require("../lib/Manager");
 
 describe("Manager", () => {
+    it("should return an object containing a 'name' property when called with the 'new' keyword", () => {
+        const obj = new Manager();
+  
+        expect("name" in obj).toEqual(true);
+})
+
+    it("should return an object containing an 'id' property when called with the 'new' keyword", () => {
+        const obj = new Manager();
+  
+        expect("id" in obj).toEqual(true);
+    })
+
+    it("should return an object containing an 'email' property when called with the 'new' keyword", () => {
+        const obj = new Manager();
+  
+        expect("email" in obj).toEqual(true);
+    })
+
+    it("should return an object containing an 'officeNumber' property when called with the 'new' keyword", () => {
+        const obj = new Manager();
+  
+        expect("officeNumber" in obj).toEqual(true);
+    })
+
+    it("should return an object containing a 'getName' method when called with the 'new' keyword", () => {
+        const obj = new Manager();
+  
+        expect(typeof obj.getName).toBe("function");
+    })
+
+    it("should return an object containing a 'getId' method when called with the 'new' keyword", () => {
+        const obj = new Manager();
+  
+        expect(typeof obj.getId).toBe("function");
+    })
+
+    it("should return an object containing a 'getEmail' method when called with the 'new' keyword", () => {
+        const obj = new Manager();
+  
+        expect(typeof obj.getEmail).toBe("function");
+    })
+
+    it("should return an object containing a 'getRole' method when called with the 'new' keyword", () => {
+        const obj = new Manager();
+  
+        expect(typeof obj.getRole).toBe("function");
+    })
+
+    it("should return the string 'Manager' when called from a new instance of Manager", () => {
+        const obj = new Manager();
+  
+        expect(obj.getRole()).toBe('Manager');
+    })
 
 });
 
