39b1632873e86b9f64569eb9859437276ea7ac90
unknown <jesseelijah.nance@gmail.com>
2022-12-28 15:57:09 -0500

    created engineer test, added code to engineer class

--- Develop/__tests__/Engineer.test.js
+++ Develop/__tests__/Engineer.test.js
@@ -2,4 +2,58 @@ const Engineer = require("../lib/Engineer");
 
 describe("Engineer", () => {
 
+    it("should return an object containing a 'name' property when called with the 'new' keyword", () => {
+        const obj = new Engineer();
+  
+        expect("name" in obj).toEqual(true);
+})
+
+    it("should return an object containing an 'id' property when called with the 'new' keyword", () => {
+        const obj = new Engineer();
+  
+        expect("id" in obj).toEqual(true);
+    })
+
+    it("should return an object containing an 'email' property when called with the 'new' keyword", () => {
+        const obj = new Engineer();
+  
+        expect("email" in obj).toEqual(true);
+    })
+
+    it("should return an object containing a 'github' property when called with the 'new' keyword", () => {
+        const obj = new Engineer();
+  
+        expect("github" in obj).toEqual(true);
+    })
+
+    it("should return an object containing a 'getName' method when called with the 'new' keyword", () => {
+        const obj = new Engineer();
+  
+        expect(typeof obj.getName).toBe("function");
+    })
+
+    it("should return an object containing a 'getId' method when called with the 'new' keyword", () => {
+        const obj = new Engineer();
+  
+        expect(typeof obj.getId).toBe("function");
+    })
+
+    it("should return an object containing a 'getEmail' method when called with the 'new' keyword", () => {
+        const obj = new Engineer();
+  
+        expect(typeof obj.getEmail).toBe("function");
+    })
+
+    it("should return an object containing a 'getRole' method when called with the 'new' keyword", () => {
+        const obj = new Engineer();
+  
+        expect(typeof obj.getRole).toBe("function");
+    })
+
+    it("should return the string 'Engineer' when called from a new instance of Engineer", () => {
+        const obj = new Engineer();
+  
+        expect(obj.getRole()).toBe('Engineer');
+    })
+
 });
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,5 +1,20 @@
 const Employee = require("./Employee");
 
-class Engineer extends Employee {}
+class Engineer extends Employee {
+
+    constructor(name, id, email, github) {
+        super(name, id, email);
+
+        this.github = github;
+    }
+
+    getGithub() {
+        return ('Github')
+    }
+
+    getRole() {
+        return ('Engineer')
+    }
+}
 
 module.exports = Engineer;
